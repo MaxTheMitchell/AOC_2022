7d4a1f251f504c03937c269dd0cb09f7ba6b160b
MaxTheMitchell <rat2043@gmail.com>
2022-11-30 21:06:54 -0800

    day 1

---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,30 @@
+import scala.io.Source
+import util.chaining.scalaUtilChainingOps
+
+@main def hello: Unit = 
+  var input = getInput
+  println("Silver")
+  silver(input)
+  println("Gold")
+  gold(input)
+
+def silver(input: String): Unit = 
+  parseInput(input)
+    .map(_.sum)
+    .max
+    .pipe(println)
+
+def gold(input: String): Unit =
+  parseInput(input)
+    .map(_.sum)
+    .sorted
+    .reverse
+    .take(3)
+    .sum
+    .pipe(println)
+
+def parseInput(input: String): Array[Array[Int]] =
+  input.split("\n\n").map(_.split("\n").map(_.toInt))
+
+def getInput: String =
+  Source.fromFile("./src/main/input/day1.txt").mkString
