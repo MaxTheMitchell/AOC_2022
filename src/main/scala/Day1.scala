2fc4994877a86f5b23065908df959230f52dbdc2
MaxTheMitchell <rat2043@gmail.com>
2022-12-02 09:17:51 -0800

    clean up code

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,30 +1,32 @@
-// import scala.io.Source
-// import util.chaining.scalaUtilChainingOps
+import scala.io.Source
+import util.chaining.scalaUtilChainingOps
 
-// @main def main: Unit = 
-//   var input = getInput
-//   println("Silver")
-//   silver(input)
-//   println("Gold")
-//   gold(input)
+object Day1 {
+    def main: Unit = 
+        var input = getInput
+        println("Silver")
+        silver(input)
+        println("Gold")
+        gold(input)
 
-// def silver(input: String): Unit = 
-//   parseInput(input)
-//     .map(_.sum)
-//     .max
-//     .pipe(println)
+    def silver(input: String): Unit = 
+    parseInput(input)
+        .map(_.sum)
+        .max
+        .pipe(println)
 
-// def gold(input: String): Unit =
-//   parseInput(input)
-//     .map(_.sum)
-//     .sorted
-//     .reverse
-//     .take(3)
-//     .sum
-//     .pipe(println)
+    def gold(input: String): Unit =
+    parseInput(input)
+        .map(_.sum)
+        .sorted
+        .reverse
+        .take(3)
+        .sum
+        .pipe(println)
 
-// def parseInput(input: String): Array[Array[Int]] =
-//   input.split("\n\n").map(_.split("\n").map(_.toInt))
+    def parseInput(input: String): Array[Array[Int]] =
+        input.split("\n\n").map(_.split("\n").map(_.toInt))
 
-// def getInput: String =
-//   Source.fromFile("./src/main/input/day1.txt").mkString
+    def getInput: String =
+        Source.fromFile("./src/main/input/day1.txt").mkString
+}
